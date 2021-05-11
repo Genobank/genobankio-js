ffc1fbd03f4057d9680968666b5ab02a078af443
Tadej Vengust <vengust.tadej@gmail.com>
2021-05-11 20:26:07 +0200

    Fic production API URL.

--- src/model/network.ts
+++ src/model/network.ts
@@ -16,7 +16,7 @@ export class Network {
     } else if (type == NetworkType.PRODUCTION) {
       this.namespacePrefix = 'io.genobank';
       this.certificateUrlBase = 'https://genobank.io/certificates/verify-certificate-v1#';
-      this.apiUrlBase = 'https://api.genobank.io/';
+      this.apiUrlBase = 'https://api.genobank.io';
       this.genoBankIoAddress = '0x633F5500A87C3DbB9c15f4D41eD5A33DacaF4184';
     } else {
       throw new Error('Unsupported network type.')
